81752ec65120be1dd4d7bf8ef7268005177a8266
anton kaptcov <kap_ton@me.com>
2013-03-21 14:53:00 +0400

    add grid selectable row

---
+++ zselecttablerow.js
@@ -0,0 +1,48 @@
+﻿/// <reference path="jquery-1.5.1.min.js" />
+
+
+(function ($) {
+	$.fn.zselecttablerow = function (options) {
+
+		var ctrlPressed = false;
+
+		var settings = $.extend({
+			stop: null
+		}, options);
+
+		return this.each(function () {
+
+			var parent = $(this);
+
+			$('body').keydown(function (evt) {
+				if (evt.ctrlKey)
+					ctrlPressed = true;
+			});
+			$('body').keyup(function () {
+				ctrlPressed = false;
+			});
+
+			$('tbody tr', parent).each(function () {
+
+				var self = $(this);
+				$(this).click(function () {
+
+					if (!ctrlPressed) {
+						$('tr', parent).each(function () {
+							$(this).removeClass('ui-selected');
+						});
+					}
+
+					if ($(self).hasClass('ui-selected'))
+						$(self).removeClass('ui-selected');
+					else
+						$(self).addClass('ui-selected');
+
+					if (settings.stop != null)
+						settings.stop();
+
+				});
+			});
+		});
+	};
+})(jQuery);
